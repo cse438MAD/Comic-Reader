f13d9aadbb4494c1354c3a85e7c3a770382a7e94
Robert Goodloe <rjgoodloe@gmail.com>
2019-04-11 15:43:42 -0500

    Added User Uploaded Comic activity

---
+++ app/src/main/java/com/example/comicreader/UserUploadedComicActivity.kt
@@ -0,0 +1,12 @@
+package com.example.comicreader
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class UserUploadedComicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_uploaded_comic)
+    }
+}
