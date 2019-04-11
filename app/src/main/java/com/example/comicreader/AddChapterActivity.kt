5e6021bff99246e2864d37dea0bf1cbd382dfb05
Robert Goodloe <rjgoodloe@gmail.com>
2019-04-11 15:48:43 -0500

    Added User Uploaded Comic activity

--- app/src/main/java/com/example/comicreader/AddChapterActivity.kt
+++ app/src/main/java/com/example/comicreader/AddChapterActivity.kt
@@ -15,6 +15,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.Toast
 import com.google.firebase.FirebaseApp
 import com.google.firebase.storage.FirebaseStorage
